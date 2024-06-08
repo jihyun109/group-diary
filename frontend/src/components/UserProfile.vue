b67472b5cd5fd5f589eb828ec52cd3d3c733e8a9
jihyun109 <wlguswlgus989@gmail.com>
2024-06-08 10:17:18 +0900

    [frontend] 유저 프로필 스타일 만들기

---
+++ frontend/src/components/UserProfile.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  props: {
+    userData: Object
+  }, 
+  methods: {
+    getInitials(name){
+      return name.substring(0, 2).toUpperCase();
+    }
+  }
+};
+</script>
+
+<template>
+    <div class="profile-circle" :style="{ backgroundColor: team.color }">
+              {{ getInitials(userData.last_name) }}
+    </div>
+</template>
