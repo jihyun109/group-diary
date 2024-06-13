daf3f3e229106290ab7ff952065babb84805f8d8
jihyun109 <wlguswlgus989@gmail.com>
2024-06-13 19:52:01 +0900

    [backend] 해당 팀의 모든 member 조회 API에 user의 color 조회 추가

--- backend/src/main/java/com/example/demo/response/TeamMembersNameResponse.java
+++ backend/src/main/java/com/example/demo/response/TeamMembersNameResponse.java
@@ -5,27 +5,38 @@ public class TeamMembersNameResponse {
     int user_id;
     String name;    // userName
 
+    String color;
+
     public TeamMembersNameResponse() {
     }
 
-    public TeamMembersNameResponse(int user_id, String name) {
+    public TeamMembersNameResponse(int user_id, String name, String color) {
         this.user_id = user_id;
         this.name = name;
+        this.color = color;
     }
 
     public int getUser_id() {
         return user_id;
     }
 
-    public String getName() {
-        return name;
-    }
-
     public void setUser_id(int user_id) {
         this.user_id = user_id;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 }
