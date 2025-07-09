5d30ccf766f61ecffbc6df059d94b56b61a7b99c
jihyun109 <wlguswlgus989@gmail.com>
2025-07-09 20:49:56 +0900

    [backend] refactor: Java 11로 전환 및 spring-boot-starter 의존성 추가

---
+++ backend/src/main/java/com/example/demo/config/JpaConfig.java
@@ -0,0 +1,11 @@
+//package com.example.demo.config;
+//
+//@Configuration
+//@EnableJpaRepositories(
+//        basePackages = "com.example.repository.jpa", // 새 JPA Repository 위치
+//        entityManagerFactoryRef = "jpaEntityManager",
+//        transactionManagerRef = "jpaTransactionManager"
+//)
+//public class JpaConfig {
+//    // EntityManagerFactory, DataSource, TransactionManager 설정
+//}
