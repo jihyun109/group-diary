765bca5083dee882e400663f238035d9a8d56d6d
jihyun109 <wlguswlgus989@gmail.com>
2025-07-11 17:11:57 +0900

    [backend] chore: controller 디렉토리 생성 후 Controller 클래스 이동

--- backend/src/main/java/com/example/demo/controller/MemberController.java
+++ backend/src/main/java/com/example/demo/controller/MemberController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.controller;
 
 import com.example.demo.member.MemberModel;
 import com.example.demo.member.MemberService;
--- backend/src/main/java/com/example/demo/controller/UserController.java
+++ backend/src/main/java/com/example/demo/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.controller;
 
 import com.example.demo.dto.LoginResponseDTO;
 import com.example.demo.entity.UserEntity;
