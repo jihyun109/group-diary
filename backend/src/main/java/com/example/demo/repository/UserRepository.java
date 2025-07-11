b959fb0ec181c6027c457b63ae00f24af7cb71b8
jihyun109 <wlguswlgus989@gmail.com>
2025-07-11 22:24:24 +0900

    [backend] feat: userId로 사용자 정보를 조회하는 API 추가

--- backend/src/main/java/com/example/demo/repository/UserRepository.java
+++ backend/src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.repository;
 
 import com.example.demo.dto.LoginResponseDTO;
 import com.example.demo.dto.LogInRequestDTO;
+import com.example.demo.dto.UserResponseDTO;
 import com.example.demo.dto.UserUpdateRequestDTO;
 import com.example.demo.user.UserModel;
 
@@ -11,4 +12,5 @@ public interface UserRepository {
     LoginResponseDTO login(LogInRequestDTO logInRequestDTO);
     void updateUser(UserUpdateRequestDTO userUpdateRequestDTO);
     List<UserModel> getUsers();
+    UserResponseDTO getUser(long userId);
 }
