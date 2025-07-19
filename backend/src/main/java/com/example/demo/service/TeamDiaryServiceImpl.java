05801594919563d4bc7888ae7a093f501c9cd0e6
jihyun109 <wlguswlgus989@gmail.com>
2025-07-19 15:41:51 +0900

    [backend] refactor: TeamDiary 도메인 Service 계층의 Mapper 의존성을 Repository로 변경

--- backend/src/main/java/com/example/demo/service/TeamDiaryServiceImpl.java
+++ backend/src/main/java/com/example/demo/service/TeamDiaryServiceImpl.java
@@ -1,9 +1,9 @@
 package com.example.demo.service;
 
 import com.example.demo.dto.TeamDiaryPostRequestDTO;
+import com.example.demo.repository.TeamDiaryRepository;
 import com.example.demo.response.SharedTeamsResponse;
 import com.example.demo.response.TeamDiaryListResponse;
-import com.example.demo.teamDiary.TeamDiaryMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,29 +12,29 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class TeamDiaryServiceImpl implements TeamDiaryService {
-    private final TeamDiaryMapper TeamDiaryMapper;
+    private final TeamDiaryRepository teamDiaryRepository;
 
     // 팁 일기 생성
     @Override
     public void insertTeamDiary(TeamDiaryPostRequestDTO teamDiary) {
-        TeamDiaryMapper.insertTeamDiary(teamDiary);
+        teamDiaryRepository.insertTeamDiary(teamDiary);
     }
 
     // 팀 일기 삭제 (공유 해제)
     @Override
     public void deleteTeamDiary(long diaryId, long teamId) {
-        TeamDiaryMapper.deleteTeamDiary(diaryId, teamId);
+        teamDiaryRepository.deleteTeamDiary(diaryId, teamId);
     }
 
     // 현재 팀에 공유된 일기 리스트 요청
     @Override
     public List<TeamDiaryListResponse> requestTeamDiaryList(long teamId) {
-        return TeamDiaryMapper.requestTeamDiaryList(teamId);
+        return teamDiaryRepository.requestTeamDiaryList(teamId);
     }
 
     // 선택한 일기가 공유된 팀들의 id, 이름 요청
     @Override
     public List<SharedTeamsResponse> requestSharedTeams(long diaryId) {
-        return TeamDiaryMapper.requestSharedTeams(diaryId);
+        return teamDiaryRepository.requestSharedTeams(diaryId);
     }
 }
