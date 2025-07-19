4cff55b1b128b3a908c9dc00063b470f1fed1ca7
jihyun109 <wlguswlgus989@gmail.com>
2025-07-19 15:16:06 +0900

    [backend] refactor: TeamDiary 도메인 Service interface 생성, TeamDiaryServiceImpl가 implement하도록 변경

--- backend/src/main/java/com/example/demo/service/TeamDiaryServiceImpl.java
+++ backend/src/main/java/com/example/demo/service/TeamDiaryServiceImpl.java
@@ -15,11 +15,13 @@ public class TeamDiaryServiceImpl implements TeamDiaryService {
     private final TeamDiaryMapper TeamDiaryMapper;
 
     // 모든 팀 일기 리스트 조회
+    @Override
     public List<TeamDiaryModel> getTeamDiaries() {
         return TeamDiaryMapper.selectTeamDiaries();
     }
 
     // 팁 일기 생성
+    @Override
     public void insertTeamDiary(TeamDiaryModel teamDiary) {
         TeamDiaryMapper.insertTeamDiary(teamDiary);
     }
@@ -31,16 +33,19 @@ public class TeamDiaryServiceImpl implements TeamDiaryService {
     }
 
     // 팀 일기 삭제 (공유 해제)
+    @Override
     public void deleteTeamDiary(int diaryId, int teamId) {
         TeamDiaryMapper.deleteTeamDiary(diaryId, teamId);
     }
 
     // 현재 팀에 공유된 일기 리스트 요청
+    @Override
     public List<TeamDiaryListResponse> requestTeamDiaryList(int teamId) {
         return TeamDiaryMapper.requestTeamDiaryList(teamId);
     }
 
     // 선택한 일기가 공유된 팀들의 id, 이름 요청
+    @Override
     public List<SharedTeamsResponse> requestSharedTeams(int diaryId) {
         return TeamDiaryMapper.requestSharedTeams(diaryId);
     }
