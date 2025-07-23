e7ea8ac765fb491f84d840849e51d4b91c2f33ac
jihyun109 <wlguswlgus989@gmail.com>
2025-07-23 21:59:18 +0900

    [backend] chore: 임시적으로 UserJPARepository 주석처리

--- backend/src/main/java/com/example/demo/jpa/UserJPARepository.java
+++ backend/src/main/java/com/example/demo/jpa/UserJPARepository.java
@@ -1,11 +1,11 @@
-package com.example.demo.jpa;
-
-import com.example.demo.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserJPARepository extends JpaRepository<UserEntity, Long> {
-    UserEntity findAllByEmailAndPassword(long email, String password);
-
-}
+//package com.example.demo.jpa;
+//
+//import com.example.demo.entity.UserEntity;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface UserJPARepository extends JpaRepository<UserEntity, Long> {
+//    UserEntity findAllByEmailAndPassword(long email, String password);
+//
+//}
