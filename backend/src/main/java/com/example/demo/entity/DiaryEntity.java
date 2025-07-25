93cf141d99cf1fb61560eec35380315a80835901
jihyun109 <wlguswlgus989@gmail.com>
2025-07-25 20:08:16 +0900

    [backend] refactor: entity 클래스에 JPA 어노테이션(@Entity, @Id) 추가

--- backend/src/main/java/com/example/demo/entity/DiaryEntity.java
+++ backend/src/main/java/com/example/demo/entity/DiaryEntity.java
@@ -1,6 +1,8 @@
 package com.example.demo.entity;
 
 import com.example.demo.model.TeamModel;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,16 +13,13 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class DiaryEntity {
     // 공유할 그룹
+    @Id
     private long id;
     private String writtenDate;
     private long writerId;
     private String diaryTitle;
     private String details;
-
-    private String firstName;
-    private String lastName;
-
-    private List<TeamModel> sharedTeamList = new ArrayList<>();
 }
--- backend/src/main/java/com/example/demo/entity/TeamEntity.java
+++ backend/src/main/java/com/example/demo/entity/TeamEntity.java
@@ -1,5 +1,7 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,7 +9,9 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class TeamEntity {
+    @Id
     private long id;
     private String team_name;
     private long creator_id;
