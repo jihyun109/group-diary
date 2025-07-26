a114703de850d78b05f8ffacb1b8f847d1eba379
jihyun109 <wlguswlgus989@gmail.com>
2025-07-26 10:41:14 +0900

    [backend] chore: TeamDiaryJPARepository 생성

---
+++ backend/src/main/java/com/example/demo/jpa/TeamDiaryJPARepository.java
@@ -0,0 +1,45 @@
+package com.example.demo.jpa;
+
+import com.example.demo.entity.TeamDiaryEntity;
+import com.example.demo.response.SharedTeamsResponse;
+import com.example.demo.response.TeamDiaryListResponse;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface TeamDiaryJPARepository extends JpaRepository<TeamDiaryEntity, Long> {
+    @Query("""
+                SELECT new com.example.demo.response.TeamDiaryListResponse(
+                    d.id,
+                    u.firstName,
+                    u.lastName,
+                    u.initial,
+                    u.color,
+                    d.writtenDate,
+                    d.diaryTitle
+                )
+                FROM TeamDiaryEntity td
+                JOIN DiaryEntity d ON td.diary_id = d.id
+                JOIN UserEntity u ON d.writerId = u.id
+                WHERE td.team_id = :teamId
+            """)
+    List<TeamDiaryListResponse> findTeamDiariesByTeamId(Long teamId);
+
+    @Query("""
+                SELECT new com.example.demo.response.SharedTeamsResponse(
+                    td.team_id,
+                    t.team_name
+                )
+                FROM TeamDiaryEntity td
+                JOIN TeamEntity t ON td.team_id = t.id
+                WHERE td.diaryId = :diaryId
+            """)
+    List<SharedTeamsResponse> findSharedTeamsByDiaryId(Long diaryId);
+
+    @Query("""
+                DELETE FROM TeamDiaryEntity td
+                WHERE td.diary_id = :diaryId AND td.team_id = :teamId
+            """)
+    void deleteByDiaryIdAndTeamId(Long diaryId, Long teamId);
+}
