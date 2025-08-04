3fe234c8e388c5782f9acd350e58d0a6460a727f
jihyun109 <wlguswlgus989@gmail.com>
2025-08-04 17:25:47 +0900

    [backend] feat: 일기 공유 요청 DTO v2 생성

---
+++ backend/src/main/java/com/example/demo/dto/DiaryEditRequestDTOv2.java
@@ -0,0 +1,14 @@
+package com.example.demo.dto;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class DiaryEditRequestDTOv2 {
+    private Long id;
+    private String title;
+    private String details;
+    private List<Long> addedTeamIds;
+    private List<Long> removedTeamIds;
+}
